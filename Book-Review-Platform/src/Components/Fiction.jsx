f95732c4389d7d909cf3fc38642034a32597ac0f
AShiSH9985 <ashish8979778599@gmail.com>
2025-07-09 20:25:38 +0530

    add fiction

---
+++ Book-Review-Platform/src/Components/Fiction.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Fiction = () => {
+  return (
+    <>
+        <div className='w-full h-80 bg-white grid grid-cols-3 items-center justify-center'>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+            <div className="h-32 w-32 bg-green-900 ">
+
+            </div>
+
+
+        </div>
+    </>
+  )
+}
+
+export default Fiction
--- Book-Review-Platform/src/Components/Home.jsx
+++ Book-Review-Platform/src/Components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import Modal from './Modal'
+import Fiction from './Fiction';
 
 function Home() {
     const [showModal, setShowModal] = React.useState(false);
@@ -9,9 +10,9 @@ function Home() {
 
     <>
         <nav className='h-16 w-full bg-[#80ba27] flex items-center justify-between px-4'>
-            <div className="w-1/2 flex items-center justify-between">
+            <div className="w-2/3 flex items-center justify-between">
                 <NavLink to='/' className='text-black text-xl m-2 m-l-8'>Book_review.com</NavLink>
-                <input type="text" onClick={()=>setShowModal(true)} className='text-black h-10 w-96 p-4 rounded-md' placeholder='Search...' />
+                <input type="text" onClick={()=>setShowModal(true)} className='text-black h-10 w-80 p-4 rounded-md' placeholder='Search...' />
             </div>
             <div className="w-1/2 flex items-center justify-end">
                 <button className="text-white b h-10 w-16 rounded-md font-semibold ">Sign in</button>
@@ -99,6 +100,7 @@ function Home() {
         </footer>
 
         {showModal && <Modal onClose={()=>setShowModal(false)} />}
+            {/* <Fiction /> */}
     </>
   )
 }
